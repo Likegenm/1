b044de27bccaceec95b86aa78650528d3a097c3f
Likegenm <Oleg.25478@mail.ru>
2025-12-13 14:40:41 +0300

    Update Zip1.lua

--- Zip1.lua
+++ Zip1.lua
@@ -1,5 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Real-Scripts/refs/heads/main/DownoloadLiblary.lua"))()
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Test/refs/heads/main/Irina.lua"))()
+
 local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua"))()
 
 local Window = WindUI:CreateWindow({
