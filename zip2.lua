9c09919a7e4049ad810bdba34a6c05ed4470790e
Likegenm <Oleg.25478@mail.ru>
2025-12-18 20:00:50 +0300

    Update zip2.lua

--- zip2.lua
+++ zip2.lua
@@ -11,7 +11,37 @@ Likegenm scripts (LS) Project scp 2.0
 by likegenm
 ]]
 
+repeat task.wait() until game:IsLoaded()
+
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/Library.lua"))()
+local function checkExecutor()
+    local executors = {
+        ["Delta"] = is_sirhurt_closure,
+        ["Synapse X"] = is_synapse_function,
+        ["ProtoSmasher"] = pebc_instance,
+        ["Krnl"] = KRNL_LOADED,
+        ["ScriptWare"] = isexecutorclosure,
+        ["Xeno"] = identifyexecutor,
+        ["Solara"] = is_solara
+        ["Wave"] = is_wave
+        ['Cryptic'] = is_cryptic
+    }
+    
+    for name, check in pairs(executors) do
+        if check then
+            Library:Notify({
+                Title = "Cheat Engine",
+                Description = "Ur executor is 90>" name, " UNC"
+                Time = 5
+            })
+            return
+        end
+    end
+end
+
+checkExecutor()
+
 local Window = Library:CreateWindow({
     Title = "PSCP",
     Footer = "v2.0 (by Likegenm)",
@@ -28,44 +58,4 @@ local MainTab = Window:AddTab("Main", "home")
 local LeftGroupbox = MainTab:AddLeftGroupbox("LocalPlayer")
 local RightGroupbox = MainTab:AddRightGroupbox("Players")
 
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
-local player = Players.LocalPlayer
-
-local teleportEnabled = false
-local teleportSpeed = 0.02
-local teleportInterval = 0.01
-
-local SpeedToggle = LeftGroupbox:AddToggle("SpeedBoost", {
-    Text = "SpeedBoost",
-    Default = false,
-    Tooltip = "SpeedBoost",
-    Callback = function(Value)
-        teleportEnabled = Value
-    end
-})
-
-SpeedToggle:OnChanged(function(Value)
-    teleportEnabled = Value
-end)
-
-local function teleportLoop()
-    if teleportEnabled and player.Character then
-        local character = player.Character
-        local humanoid = character:FindFirstChild("Humanoid")
-        local rootPart = character:FindFirstChild("HumanoidRootPart")
-        
-        if humanoid and rootPart then
-            local moveDirection = humanoid.MoveDirection
-            
-            if moveDirection.Magnitude > 0 then
-                local newPosition = rootPart.Position + (moveDirection * teleportSpeed)
-                rootPart.CFrame = CFrame.new(newPosition)
-            end
-        end
-    end
-end
-
-RunService.Heartbeat:Connect(function()
-    teleportLoop()
-end)
+local 
