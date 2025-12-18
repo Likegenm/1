16eef2eb6b797bafcb7de305eff7735e3da01a3b
Likegenm <Oleg.25478@mail.ru>
2025-12-18 21:22:22 +0300

    Update zip2.lua

--- zip2.lua
+++ zip2.lua
@@ -13,49 +13,6 @@ by likegenm
 
 repeat task.wait() until game:IsLoaded()
 
-local StarterGui = game:GetService("StarterGui")
-
-local function checkExecutor()
-    local executors = {
-        ["Delta"] = is_sirhurt_closure,
-        ["Synapse X"] = is_synapse_function,
-        ["ProtoSmasher"] = pebc_instance,
-        ["Krnl"] = KRNL_LOADED,
-        ["ScriptWare"] = isexecutorclosure,
-        ["Xeno"] = identifyexecutor,
-        ["Solara"] = is_solara,
-        ["Wave"] = is_wave,
-        ["Cryptic"] = is_cryptic
-    }
-    
-    for name, check in pairs(executors) do
-        if check then
-           StarterGui:SetCore("SendNotification", {
-                Title = "Cheat Engine",
-                Text = "Ur executor 90>ur executor",
-                Icon = "",
-                Duration = 5,
-                Button1 = "OK",
-                Callback = function()
-                end
-           })
-           return
-        end
-    end
-end
-
-checkExecutor()
-
-
---[[
-██╗     ██╗██╗██╗  ██╗███████╗ ██████╗ ███████╗███╗   ██╗███╗   ███╗
-██║     ██║██║██║ ██╔╝██╔════╝██╔════╝ ██╔════╝████╗  ██║████╗ ████║
-██║     ██║██║█████╔╝ █████╗  ██║  ███╗█████╗  ██╔██╗ ██║██╔████╔██║
-██║     ██║██║██╔═██╗ ██╔══╝  ██║   ██║██╔══╝  ██║╚██╗██║██║╚██╔╝██║
-███████╗██║██║██║  ██╗███████╗╚██████╔╝███████╗██║ ╚████║██║ ╚═╝ ██║
-╚══════╝╚═╝╚═╝╚═╝  ╚═╝╚══════╝ ╚═════╝ ╚══════╝╚═╝  ╚═══╝╚═╝     ╚═╝
-]]
-
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/deividcomsono/Obsidian/refs/heads/main/Library.lua"))()
 
 local Window = Library:CreateWindow({
