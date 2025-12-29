cde0ae3bd09a677fe3443899646fc8596c012916
Likegenm <Oleg.25478@mail.ru>
2025-12-29 14:50:10 +0300

    Create zip5.lua

---
+++ zip5.lua
@@ -0,0 +1,33 @@
+local WindUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/Footagesus/WindUI/main/dist/main.lua"))()
+
+local Window = WindUI:createWindow({
+    Title = "Magic Tulevo script",
+    Author = "Likegenm
+    Folder = "AxmedHub",
+    Icon = "code",
+    IconSize = 22*2,
+    NewElements = true,
+    HideSearchBar = false,
+    OpenButton = {
+        Title = "UI",
+        CornerRadius = UDim.new(1,0),
+        StrokeThickness = 3,
+        Enabled = true,
+        Draggable = true,
+        OnlyMobile = false,
+        
+        Color = ColorSequence.new(
+            Color3.fromHex("#ff0000"), 
+            Color3.fromHex("#ffa500")
+            Color3.fromHex("#ffff00")
+            Color3.fromHex("#008000")
+            Color3.fromHex("#42aaff")
+            Color3.fromHex("#0000ff")
+            Color3.fromHex("#8b00ff")
+        )
+    },
+    Topbar = {
+        Height = 44,
+        ButtonsType = "Mac",
+    },
+  
