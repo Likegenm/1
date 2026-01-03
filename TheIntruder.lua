7afd81b8eedb03a1a1c069e46d640e15bac32322
Likegenm <Oleg.25478@mail.ru>
2026-01-03 20:48:48 +0300

    Update TheIntruder.lua

--- TheIntruder.lua
+++ TheIntruder.lua
@@ -1,3 +1,3 @@
-local PG = game:GetService("Players").LocalPlayer.PlayerGui
-local lobby = PG.Room
+local PG = game:GetService("Players").LocalPlayer.PlayerGui.Rooms
+local lobby = game:GetService("Players").LocalPlayer.PlayerGui.Room
 local home = PG.Basement and PG.BasementStairs and PG.Bathroom and PG.FrontDoor and PG.Hallway and PG.LivingRoom and PG.Outside and PG.Street and PG.Vent
