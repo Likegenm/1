aeb7fc2db1392e9af0a4d9f573a1fd9afacbb3c0
Likegenm <Oleg.25478@mail.ru>
2026-01-04 18:48:22 +0300

    Update TheIntruder.lua

--- TheIntruder.lua
+++ TheIntruder.lua
@@ -1,7 +1,7 @@
 local PG = game:GetService("Players").LocalPlayer.PlayerGui.Rooms
 local LK = game.workspace.Lobby
 local sc = game.workspace.Cameras
-local lobby = game:GetService("Players").LocalPlayer.PlayerGui.Room
+local lobby = game.Players.LocalPlayer.PlayerGui.Room
 local home = PG.Basement and PG.BasementStairs and PG.Bathroom and PG.FrontDoor and PG.Hallway and PG.LivingRoom and PG.Outside and PG.Street and PG.Vent
 local Hallways = PG.Hallway1 and PG.Hallway2 and PG.Hallway3
 local mall = PG.Diner and PG.Escalator and PG.Hallway and PG.Hallway2 and PG.Hallway3 and PG.Janitor and PG.Outside and PG.SecondFloor and PG.ShoeStore
