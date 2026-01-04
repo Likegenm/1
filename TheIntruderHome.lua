af516f96753172f8c1a57b1cc1b0e90d14156372
Likegenm <Oleg.25478@mail.ru>
2026-01-04 12:23:09 +0300

    Update TheIntruderHome.lua

--- TheIntruderHome.lua
+++ TheIntruderHome.lua
@@ -6,12 +6,12 @@ local IntruderPos = game:GetService("Workspace").Values.intruderPos
 local PlrAnim = game:GetService("Players").LocalPlayer.Character.Animate
 local CamMovement = game:GetService("Players").LocalPlayer.Character.RealisticCameraMovement
 local StupidBook = game:GetService("Players").Book
-local function InteractClick(Seconds)
-  game:GetService("Workspace").Map.Speaker.AnswerGuide.HoldDuration = Seconds
-  game:GetService("Workspace").Map.Speaker.AnswerGuide2.HoldDuration = Seconds
-  game:GetService("Workspace").Map.Speaker.FixPhone.HoldDuration = Seconds
-  game:GetSevice("Workspace").Map.LightSwitch.DownSwitch.ProximityPrompt.HoldDuration = Seconds
-  game:GetSevice("Workspace").Map.LightSwitch.UpSwitch.ProximityPrompt.HoldDuration = Seconds
-  game:GetService("Workspace").Map.ClosetDoor.Handle.Close.HoldDuration = Seconds
-  game:GetService("Workspace").Map.ClosetDoor.Handle.Open.HoldDuration = Seconds
+local function InteractClick()
+  game:GetService("Workspace").Map.Speaker.AnswerGuide.HoldDuration = 0
+  game:GetService("Workspace").Map.Speaker.AnswerGuide2.HoldDuration = 0
+  game:GetService("Workspace").Map.Speaker.FixPhone.HoldDuration = 0
+  game:GetSevice("Workspace").Map.LightSwitch.DownSwitch.ProximityPrompt.HoldDuration = 0
+  game:GetSevice("Workspace").Map.LightSwitch.UpSwitch.ProximityPrompt.HoldDuration = 0
+  game:GetService("Workspace").Map.ClosetDoor.Handle.Close.HoldDuration = 0
+  game:GetService("Workspace").Map.ClosetDoor.Handle.Open.HoldDuration = 0
 end
