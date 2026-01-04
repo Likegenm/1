22ed5e2e443caeac29bb8b610f6dbc2147940952
Likegenm <Oleg.25478@mail.ru>
2026-01-04 18:51:45 +0300

    Update TheIntruder.lua

--- TheIntruder.lua
+++ TheIntruder.lua
@@ -1,14 +1,72 @@
-local PG = game:GetService("Players").LocalPlayer.PlayerGui.Rooms
-local LK = game.workspace.Lobby
-local sc = game.workspace.Cameras
-local lobby = game.Players.LocalPlayer.PlayerGui.Room
-local home = PG.Basement and PG.BasementStairs and PG.Bathroom and PG.FrontDoor and PG.Hallway and PG.LivingRoom and PG.Outside and PG.Street and PG.Vent
-local Hallways = PG.Hallway1 and PG.Hallway2 and PG.Hallway3
-local mall = PG.Diner and PG.Escalator and PG.Hallway and PG.Hallway2 and PG.Hallway3 and PG.Janitor and PG.Outside and PG.SecondFloor and PG.ShoeStore
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local PG = LocalPlayer.PlayerGui:FindFirstChild("Rooms")
+local LK = workspace:FindFirstChild("Lobby")
+local sc = workspace:FindFirstChild("Cameras")
+
+local lobby = LocalPlayer.PlayerGui:FindFirstChild("Room")
+
+local home = PG and 
+           PG:FindFirstChild("Basement") and 
+           PG:FindFirstChild("BasementStairs") and 
+           PG:FindFirstChild("Bathroom") and 
+           PG:FindFirstChild("FrontDoor") and 
+           PG:FindFirstChild("Hallway") and 
+           PG:FindFirstChild("LivingRoom") and 
+           PG:FindFirstChild("Outside") and 
+           PG:FindFirstChild("Street") and 
+           PG:FindFirstChild("Vent")
+
+local Hallways = PG and 
+                PG:FindFirstChild("Hallway1") and 
+                PG:FindFirstChild("Hallway2") and 
+                PG:FindFirstChild("Hallway3")
+
+local mall = PG and 
+            PG:FindFirstChild("Diner") and 
+            PG:FindFirstChild("Escalator") and 
+            PG:FindFirstChild("Hallway") and 
+            PG:FindFirstChild("Hallway2") and 
+            PG:FindFirstChild("Hallway3") and 
+            PG:FindFirstChild("Janitor") and 
+            PG:FindFirstChild("Outside") and 
+            PG:FindFirstChild("SecondFloor") and 
+            PG:FindFirstChild("ShoeStore")
+
 local mine = not PG and not lobby
-local MH = Hallways and PG.North and PG.Storage and PG.Study and PG.Vent
-local SL = sc.1 and sc.2 and sc.3 and sc.4 and sc.5
-local OAK = LK.OldMan and LK.Woman and LK.Worker and LK.HackingTool
+
+local MH = Hallways and 
+          PG:FindFirstChild("North") and 
+          PG:FindFirstChild("Storage") and 
+          PG:FindFirstChild("Study") and 
+          PG:FindFirstChild("Vent")
+
+
+local SL = sc and 
+          sc:FindFirstChild("1") and 
+          sc:FindFirstChild("2") and 
+          sc:FindFirstChild("3") and 
+          sc:FindFirstChild("4") and 
+          sc:FindFirstChild("5")
+
+local OAK = LK and 
+           LK:FindFirstChild("OldMan") and 
+           LK:FindFirstChild("Woman") and 
+           LK:FindFirstChild("Worker") and 
+           LK:FindFirstChild("HackingTool")
+
 local Subway = not mine and not PG and not lobby
-local DM = PG.Basement and PG.FrontDoor and PG.Hallway and PG.LivingRoom and PG.Mall and PG.Outside and PG.Secret and PG.Well
-if lobby then loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/Intruder(Lobby).lua"))() end
+
+local DM = PG and 
+          PG:FindFirstChild("Basement") and 
+          PG:FindFirstChild("FrontDoor") and 
+          PG:FindFirstChild("Hallway") and 
+          PG:FindFirstChild("LivingRoom") and 
+          PG:FindFirstChild("Mall") and 
+          PG:FindFirstChild("Outside") and 
+          PG:FindFirstChild("Secret") and 
+          PG:FindFirstChild("Well")
+
+if lobby then 
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Likegenm/Scripts/refs/heads/main/Intruder(Lobby).lua"))()
+end
