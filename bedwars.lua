d7c95923b7e443cb0cba2959377aaebe543412bf
Likegenm <Oleg.25478@mail.ru>
2026-01-17 19:14:52 +0300

    Update bedwars.lua

--- bedwars.lua
+++ bedwars.lua
@@ -17,3 +17,72 @@ local IT = Window:AddTab('Info')
 IT:AddLeftGroupbox('Credits'):AddLabel('Main:Likegenm')
 
 IT:AddLeftGroupbox('Credits'):AddLabel('Scripter:Likegenm')
+
+local PT = Window:AddTab('Player')
+
+local SGB = PT:AddLeftGroupbox('Speed')
+
+SGB:AddSlider('Speed', {
+        Text = 'Speed',
+        Default = 21,
+        Min = 20,
+        Max = 23,
+        Rounding = 1,
+        Compact = true,
+        Callback = function(Value)
+
+end
+})
+
+Library:SetWatermarkVisibility(true)
+
+-- Example of dynamically-updating watermark with common traits (fps and ping)
+local FrameTimer = tick()
+local FrameCounter = 0;
+local FPS = 60;
+
+local WatermarkConnection = game:GetService('RunService').RenderStepped:Connect(function()
+    FrameCounter += 1;
+
+    if (tick() - FrameTimer) >= 1 then
+        FPS = FrameCounter;
+        FrameTimer = tick();
+        FrameCounter = 0;
+    end;
+
+    Library:SetWatermark(('LinoriaLib demo | %s fps | %s ms'):format(
+        math.floor(FPS),
+        math.floor(game:GetService('Stats').Network.ServerStatsItem['Data Ping']:GetValue())
+    ));
+end);
+
+Library.KeybindFrame.Visible = true; -- todo: add a function for this
+
+Library:OnUnload(function()
+    WatermarkConnection:Disconnect()
+
+    print('Unloaded!')
+    Library.Unloaded = true
+end)
+
+local MenuGroup = WIndow:AddTab('UI'):AddLeftGroupbox('Menu')
+
+MenuGroup:AddButton('Unload', function() Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+
+Library.ToggleKeybind = Options.MenuKeybind
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+
+SaveManager:IgnoreThemeSettings()
+
+SaveManager:SetIgnoreIndexes({ 'MenuKeybind' })
+
+ThemeManager:SetFolder('MyScriptHub')
+SaveManager:SetFolder('MyScriptHub/specific-game')
+
+SaveManager:BuildConfigSection(Tabs['UI Settings'])
+
+ThemeManager:ApplyToTab(Tabs['UI Settings'])
+
+SaveManager:LoadAutoloadConfig()
