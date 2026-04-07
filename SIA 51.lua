5b25e9dbc64fb1f9a5565d78590a7fa37b56d9e2
Likegenm <Oleg.25478@mail.ru>
2026-04-07 18:44:04 +0300

    Create SIA 51.lua

---
+++ SIA 51.lua
@@ -0,0 +1,28 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
+local Window = Rayfield:CreateWindow({
+   Name = "SIA 51",
+   LoadingTitle = "by Likegenm + Vicinly",
+   LoadingSubtitle = "Helper Library: RedStar",
+   ConfigurationSaving = {
+      Enabled = false,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "Pornhub"
+   },
+   Discord = {
+      Enabled = false,
+      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+   }
+})
+
+local Tab = Window:CreateTab("Main", 4483362458) -- Title, Image
+
+local Bypass = Tab:CreateSection("Bypasser")
+
+local BypassButton = Bypass:CreateButton({
+   Name = "Bypass AntiCheat",
+   Callback = function()
+	
+   end,
+})  
