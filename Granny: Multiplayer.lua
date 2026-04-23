2e0c60960513164f3dcb2e1a59b41a5d60340a72
Likegenm <Oleg.25478@mail.ru>
2026-04-23 20:44:58 +0300

    Update Granny: Multiplayer.lua

--- Granny: Multiplayer.lua
+++ Granny: Multiplayer.lua
@@ -1,3 +1,13 @@
 local chapter1 = game:GetService("Workspace").Preset4
 local chapter2 = game:GetService("Workspace").Preset5
 local chapter3 = game:GetService("Workspace").Preset1
+
+if chapter1 then
+  print("Chapter 1")
+end
+if chapter2 then
+  print("Chapter 2")
+end
+if chapter3 then
+  print("Chapter 3")
+end
