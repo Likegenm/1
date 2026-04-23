c47987a1c531021eb44df0fe7e799c3bc2adb646
Likegenm <Oleg.25478@mail.ru>
2026-04-23 20:41:13 +0300

    Update Granny: Multiplayer.lua

--- Granny: Multiplayer.lua
+++ Granny: Multiplayer.lua
@@ -1,3 +1,3 @@
 local chapter1 = game:GetService("Workspace").Preset4.Locks.SlendrinaMother
-local chapter2 = 
+local chapter2 = game:GetService("Workspace").Preset4.Locks.Grandpa
 locla chapter3 = 
